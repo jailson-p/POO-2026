ae445618509eeaa16ef9f50389b8af79399e1542
Jailson <jailson.p@aluno.ifsc.edu.br>
2026-03-24 11:27:56 -0300

    lab-03, metodo porextenso em construção

--- lab-03/Horario/src/main/java/engetelecom/poo/Horario.java
+++ lab-03/Horario/src/main/java/engetelecom/poo/Horario.java
@@ -36,16 +36,42 @@ public class Horario {
     }
 
     public StringBuilder porExtenso(){
-
-    }
-    private String conversaoExtenso(int tempo){
-        String textoNum = new String(String.format("%02d",tempo)) ;
-        switch (textoNum.charAt(0)){
-            case '0': return "zero";
-            case
-
+        StringBuilder extenso= new StringBuilder();
+        int[] horarioMatriz = new int[]{this.hora,this.minuto,this.segundo};
+        for (int tempo : horarioMatriz) {
+            boolean menor20 = tempo < 20 ? true : false;
+            int dezena = menor20 ? 0 : (tempo / 10) * 10 ;
+            int unidade = menor20 ? tempo : tempo % 10;
+            switch (tempo) {
+                case 0: extenso.append(" zero ");            break;
+                case 1: extenso.append(" um ");         break;
+                case 2: extenso.append(" dois ");       break;
+                case 3: extenso.append(" três ");       break;
+                case 4: extenso.append(" quatro ");     break;
+                case 5: extenso.append(" cinco ");      break;
+                case 6: extenso.append(" seis ");       break;
+                case 7: extenso.append(" sete ");       break;
+                case 8: extenso.append(" oito ");       break;
+                case 9: extenso.append(" nove ");       break;
+                case 10: extenso.append(" dez ");       break;
+                case 11: extenso.append(" onze ");      break;
+                case 12: extenso.append(" doze ");      break;
+                case 13: extenso.append(" treze ");     break;
+                case 14: extenso.append(" quatorze ");  break;
+                case 15: extenso.append(" quinze ");    break;
+                case 16: extenso.append(" dezeseis ");  break;
+                case 17: extenso.append(" dezesete ");  break;
+                case 18: extenso.append(" dezoito ");   break;
+                case 19: extenso.append(" dezenove ");  break;
+                case 20: extenso.append(" vinte ");     break;
+                case 30: extenso.append(" trinta ");    break;
+                case 40: extenso.append(" quarenta ");  break;
+                case 50: extenso.append(" cinquenta "); break;
+            }
         }
 
+
+        return extenso.toString();
     }
 
 
